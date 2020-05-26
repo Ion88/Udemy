a6cfa1c702875319c0e92273ddbd94f761ba35f0
Ion88 <ion.vizitiu88@gmail.com>
2020-05-26 14:10:18 +0100

    Working on Responsive Adjustments

--- burger-builder-react/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder-react/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,17 @@ import React from "react";
 
 import Logo from "../../Logo/Logo";
 import classes from "./Toolbar.css";
+import NavigationItems from "./NavigationItems/NavigationItems";
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
-    <nav>...</nav>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
