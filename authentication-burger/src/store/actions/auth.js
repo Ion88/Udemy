5b9ab22414be3be1ab7135ef55b8432582c9b4c2
Ion88 <ion.vizitiu88@gmail.com>
2021-03-23 21:31:17 +0000

    add sign in

--- authentication-burger/src/store/actions/auth.js
+++ authentication-burger/src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
@@ -20,8 +22,29 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return (dispatch) => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC0i-dzgV0o7F-F4f8DkhIPmZq24spzDuk";
+    if (!isSignup) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC0i-dzgV0o7F-F4f8DkhIPmZq24spzDuk";
+    }
+    axios
+      .post(url, authData)
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
   };
 };
