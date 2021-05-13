ed786c059e5b08fcb1c92cf4c952ed37cd1038dd
Ion88 <ion.vizitiu88@gmail.com>
2021-05-13 11:44:39 +0100

    finish module 9 - objects

--- javascript/objects-01-starting-project/assets/scripts/objects.js
+++ javascript/objects-01-starting-project/assets/scripts/objects.js
@@ -52,8 +52,10 @@ const renderMovies = (filter = "") => {
     const movieEl = document.createElement("li");
     const { info, ...otherProps } = movie;
     console.log(otherProps);
-    const { title: movieTitle } = info;
-    let text = movieTitle + " - ";
+    //const { title: movieTitle } = info;
+    let { getFormattedTitle } = movie;
+    //getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = movie.getFormattedTitle.call(movie) + " - ";
     for (const key in info) {
       if (key !== "title") {
         text = text + `${key}: ${info[key]}`;
@@ -83,12 +85,16 @@ const addMovieHandler = () => {
       [extraName]: extraValue,
     },
     id: Math.random().toString(),
+    getFormattedTitle() {
+      return this.info.title.toUpperCase();
+    },
   };
   movies.push(newMovie);
   renderMovies();
 };
 
 const searchMovieHandler = () => {
+  console.log(this);
   const filterTerm = document.getElementById("filter-title").value;
   renderMovies(filterTerm);
 };
