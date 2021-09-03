d4689364e519aa1c402ded3244be96d1b6130ac8
Ion88 <ion.vizitiu88@gmail.com>
2021-09-03 14:18:22 +0100

    starting section 17

--- javascript/nums-strings-01-starting-setup/app.js
+++ javascript/nums-strings-01-starting-setup/app.js
@@ -4,3 +4,21 @@ function randomNumberBetween(min, max) {
 }
 
 console.log(randomNumberBetween(5, 10));
+
+function productDescription(strings, productName, productPrice) {
+  console.log(strings);
+  console.log(productName);
+  console.log(productPrice);
+  let priceCategory = "cheap";
+  if (productPrice > 20) {
+    priceCategory = "fair";
+  }
+  //return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`;
+  return { name: productName, price: productPrice };
+}
+
+const prodName = "JavaScript Course";
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`;
+console.log(productOutput);
