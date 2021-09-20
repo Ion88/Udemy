d1ca1ab5f10d464a05652ba23ecf2d484043695b
Ion88 <ion.vizitiu88@gmail.com>
2021-09-20 22:19:35 +0100

    end of module 27

--- javascript/practice-places-01-starting-setup/src/MyPlace.js
+++ javascript/practice-places-01-starting-setup/src/MyPlace.js
@@ -11,9 +11,22 @@ class LoadedPlace {
 
 const url = new URL(location.href);
 const queryParam = url.searchParams;
-const coords = {
-  lat: +queryParam.get("lat"),
-  lng: +queryParam.get("lng"),
-};
-const address = queryParam.get("address");
-new LoadedPlace(coords, address);
+// const coords = {
+//   lat: +queryParam.get("lat"),
+//   lng: +queryParam.get("lng"),
+// };
+// const address = queryParam.get("address");
+const locId = queryParam.get("location");
+fetch("http://localhost:3000/location/" + locId)
+  .then((response) => {
+    if (response.status === 404) {
+      throw new Error("Could not find location!");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    new LoadedPlace(data.coordinates, data.address);
+  })
+  .catch((err) => {
+    alert(err.message);
+  });
