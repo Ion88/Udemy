ab43ba9e233d00a565bdb4e1d37e3fdea255eaac
Ion88 <ion.vizitiu88@gmail.com>
2021-10-16 11:59:40 +0100

    end of section 3, beginning

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -1,24 +1,7 @@
-const path = require('path');
+const http = require("http");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const routes = require("./routes");
 
-const errorController = require('./controllers/error');
+const server = http.createServer(routes);
 
-const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
-app.use(errorController.get404);
-
-app.listen(3000);
+server.listen(3000);
